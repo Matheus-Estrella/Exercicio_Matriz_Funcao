35d956a2019feb7abc44d0303c0c2a91ea1f59bc
Matheus-Estrella <matheus.a.estrella@gmail.com>
2024-04-24 22:23:58 -0300

    Atualização

--- Exercicio_Matriz_Funcao/Program.cs
+++ Exercicio_Matriz_Funcao/Program.cs
@@ -1,12 +1,32 @@
-﻿const int size = 5;
+﻿using System;
+
+const int size = 5;
 float[,] matriz_1 = new float[size, size];
 float[,] matriz_2 = new float[size, size];
 int lin, col;
+int lin_size = Definir_Tamanho(1);
+int Col_size = Definir_Tamanho(2);
+
+int Definir_Tamanho(int num)
+{
+    string lin_Col = "";
+    if (num == 1)
+    {
+        lin_Col = "Linha";
+    }
+    else
+    {
+        lin_Col = "Coluna";
+    }
+    Console.Write($"\nInsira a quantidade de {lin_Col} da matriz: ");
+    return int.Parse(Console.ReadLine());
+}
 float[,] Gerar_Matriz(float[,] matriz, string name)
 {
-    for (lin = 0; lin < size; lin++)
+
+    for (lin = 0; lin < lin_size; lin++)
     {
-        for (col = 0; col < size; col++)
+        for (col = 0; col < Col_size; col++)
         {
             matriz[lin, col] = new Random().Next(1, 100);
         }
@@ -17,11 +37,11 @@ float[,] Gerar_Matriz(float[,] matriz, string name)
 void Imprimir_Matriz(string operacao, float[,] matriz, int size)
 {
     Console.WriteLine($"\nImprimindo Matriz {operacao}: \n");
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < lin_size; i++)
     {
-        for (int j = 0; j < size; j++)
+        for (int j = 0; j < Col_size; j++)
         {
-            Console.Write($"{matriz[i, j]} "); ;
+            Console.Write($"{matriz[i, j]:000} "); ;
         }
         Console.WriteLine();
     }
@@ -39,9 +59,9 @@ void Menu(float[,] matriz_1, float[,] matriz_2)
             Console.Write("\nDigite um número para escolher a operação:\n1-) Soma\n2-) Subtração\n3-) Multiplicação\n4-) Divisão\n5-) Sair\n");
             op = int.Parse(Console.ReadLine());
         } while ((op < 1) || (op > 5));
-        for (lin = 0; lin < size; lin++)
+        for (lin = 0; lin < lin_size; lin++)
         {
-            for (col = 0; col < size; col++)
+            for (col = 0; col < Col_size; col++)
             {
                 switch (op)
                 {
